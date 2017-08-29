3163381e1d0594a9e38b897bc510234c4fbf7c06
Prof Felipe Antunes <felipe.antunes@ifnmg.edu.br>
2017-08-29 15:52:54 -0300

    Exercicios de consultas

---
+++ ExerciciosConsultas.js
@@ -0,0 +1,38 @@
+// -------------------------------------------------------------------
+// PASSO A PASSO - ENVIO DO EXERCÍCIO
+// -------------------------------------------------------------------
+
+// 1 - Clone o repositorio do seu usuário github
+// 	git clone https://github.com/seu-usuario/dbfilmes.git 
+// 2 - Entre na pasta que foi clonada
+//	cd pasta
+// 3 - Configura seu usuario e senha
+//	git config user.name "Seu nome"
+//	git config user.email "seuemail@email.com"
+// 4 - Crie um novo arquivo "seu-nome-exercicioConsulta.js"
+// 5 - Adicione o arquivo no git
+//	 git add seu-nome-trabalho.md
+// 6 - Comite o documento
+//	 git commit -m "Escreva uma mensagem"
+// 7 - Envie as alterações para o github
+//	 git push -u
+// 8 - Digite seu nome de usuário do github, depois digite a senha do usuário
+// 9 - No seu repositório do github, clique no botão "New pull request"
+// 10 - Depois clique no botão "Create pull request"
+
+
+//--------------------------------------------------------------------
+// EXERCICIOS CONSULTAS
+//--------------------------------------------------------------------
+
+// 01 - Filmes de ação lancados entre 2000 e 2003.
+// 02 - Filmes de comedia ou terror com menos de 2 horas.
+// 03 - Filmes onde bruce willis atuou.
+// 04 - Quantos filmes arrecadarão mais de 1 milhao.
+// 05 - Quais os diretores dos 10 filmes com maior arrecadação.
+// 06 - Filmes dirigidos por Steven Spierlberg na decada de 90.
+// 07 - Qual o filmes com mais de 3 horas que teve a maior arrecadação.
+// 08 - Quantos diretores diferentes tivemos em 2005.
+// 09 - Quais os atores de filmes de ação e comedia de 2010.
+// 10 - Qual o filme com Brad Pitt com maior duração.
+
